ac94bd8fc7ac0171ef4c2e6ac407f6281b00b23d
smissuser5 <a.mykhailovskyi@smissltd.com>
2021-09-29 23:51:28 +0300

    Add api controllers

---
+++ laravel/app/Http/Controllers/Api/ProductReviewController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use GuzzleHttp\Psr7\Response;
+use Illuminate\Http\Request;
+
+class ProductReviewController extends Controller
+{
+    /**
+     * Get the list of all products reviews
+     *
+     * @return Response
+     */
+    public function index(): Response
+    {
+        //
+    }
+
+    /**
+     * Save a product review
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function store(Request $request): Response
+    {
+        //
+    }
+
+}
