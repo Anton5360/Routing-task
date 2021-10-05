ca36f4be135a94c906a39e581a78057297a24d57
smissuser5 <a.mykhailovskyi@smissltd.com>
2021-10-05 21:31:07 +0300

    Register getting of singleton AbstractPayment instance

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -2,7 +2,12 @@
 
 namespace App\Providers;
 
+use App\Payments\ApplePayPayment;
+use App\Payments\Components\AbstractPayment;
+use App\Payments\PayPalPayment;
+use App\Payments\StripePayment;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +18,25 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(AbstractPayment::class, function() {
+
+            $paymentSystem = request('payment_system');
+
+            abort_unless($paymentSystem, '403', 'Payment system is required');
+
+            $preparedPaymentSystemName = Str::preparePaymentSystemName($paymentSystem);
+
+            switch($preparedPaymentSystemName){
+                case 'apple':
+                    return new ApplePayPayment();
+                case 'paypal':
+                    return new PayPalPayment();
+                case 'stripe':
+                    return new StripePayment();
+                default:
+                    abort('403', 'Requested payment system does not exist');
+            }
+        });
     }
 
     /**
